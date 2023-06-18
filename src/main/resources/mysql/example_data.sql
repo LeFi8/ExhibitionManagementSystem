0bb49a541c92cd604af6ff3764a18b71139f1367
lef8 <alan.sipiorski14@gmail.com>
2023-06-18 20:47:06 +0200

    modify example data file

--- src/main/resources/mysql/example_data.sql
+++ src/main/resources/mysql/example_data.sql
@@ -1,20 +1,20 @@
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Underworld Journey', '2023-06-15', '2023-08-31', 'Discover the mesmerizing beauty of the underwater world with its vibrant marine life and stunning coral reefs. Dive into an immersive experience that showcases the wonders of our oceans.', 15);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Artistic Expressions', '2023-07-10', '2023-08-15', 'Immerse yourself in a world of artistic expressions that push the boundaries of creativity. Experience a diverse range of art forms, including paintings, sculptures, installations, and digital art.', 12);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Nature Serenity', '2023-07-20', '2023-09-05', 'Indulge in the tranquility of natures serenity as you explore breathtaking landscapes, serene forests, and majestic mountains. Embark on a journey that reconnects you with the beauty of the natural world.', 18);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('History Unveiled', '2023-08-01', NULL, 'Uncover the mysteries of the past as you delve into the captivating world of history. From ancient civilizations to modern revolutions, witness the stories that shaped our world.', 14);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Dreamscapes', '2023-08-10', '2023-09-30', 'Step into a realm of dreams and imagination where reality meets fantasy. Experience surreal landscapes, magical creatures, and enchanting adventures that will transport you to another world.', 16);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Innovations Expo', '2023-09-05', '2023-09-15', 'Explore the cutting-edge innovations that are shaping the future. From technological advancements to groundbreaking discoveries, witness the latest breakthroughs in science, engineering, and industry.', 20);
 
-INSERT INTO exhibition (name, start_date, end_date, description, default_ticket_price)
+INSERT INTO exhibition (name, start_date, end_date, description, ticket_price)
 VALUES ('Cultural Diversity', '2023-09-20', '2023-10-31', 'Celebrate the rich tapestry of cultures from around the world. Immerse yourself in the traditions, customs, music, and art that define different societies and foster a sense of global unity.', 10);

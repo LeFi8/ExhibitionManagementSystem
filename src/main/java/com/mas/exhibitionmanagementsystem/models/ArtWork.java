737920eb3c21a06502d65740d524ba46c9557c91
lef8 <alan.sipiorski14@gmail.com>
2023-06-18 23:36:36 +0200

    add getters and setters

--- src/main/java/com/mas/exhibitionmanagementsystem/models/ArtWork.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/models/ArtWork.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "art_work")
+@Table(name = "artwork")
 public class ArtWork {
     @Id
     @Column(name = "id")
@@ -25,4 +25,47 @@ public class ArtWork {
 
     @ManyToMany(mappedBy = "artworks")
     private Set<Artist> artists = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCreationYear() {
+        return creationYear;
+    }
+
+    public void setCreationYear(int creationYear) {
+        this.creationYear = creationYear;
+    }
+
+    public Exhibition getExhibition() {
+        return exhibition;
+    }
+
+    public void setExhibition(Exhibition exhibition) {
+        this.exhibition = exhibition;
+    }
+
+    public String getArtistsNames() {
+        StringBuilder stringBuilder = new StringBuilder();
+        artists.forEach(artist -> {
+            stringBuilder.append(artist.getName());
+            stringBuilder.append(" ");
+            stringBuilder.append(artist.getSurname());
+            stringBuilder.append('\n');
+        });
+        return stringBuilder.toString().trim();
+    }
+
+    public void setArtists(Set<Artist> artists) {
+        this.artists = artists;
+    }
 }

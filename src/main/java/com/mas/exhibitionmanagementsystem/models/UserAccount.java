337c3a2b0fbd15472e1b18586ab1fd33d0229d02
lef8 <alan.sipiorski14@gmail.com>
2023-06-20 14:46:26 +0200

    add getters and setters

--- src/main/java/com/mas/exhibitionmanagementsystem/models/UserAccount.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/models/UserAccount.java
@@ -18,4 +18,32 @@ public class UserAccount {
 
     @OneToOne(mappedBy = "account")
     private Client client;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }

bbae8f1f9a22cebce359adaf2f68c9ff1066e4b7
lef8 <alan.sipiorski14@gmail.com>
2023-06-20 14:21:00 +0200

    create useraccount service and repository

---
+++ src/main/java/com/mas/exhibitionmanagementsystem/repositories/UserAccountRepository.java
@@ -0,0 +1,10 @@
+package com.mas.exhibitionmanagementsystem.repositories;
+
+import com.mas.exhibitionmanagementsystem.models.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    UserAccount findByEmail(String email);
+}
---
+++ src/main/java/com/mas/exhibitionmanagementsystem/services/UserAccountService.java
@@ -0,0 +1,49 @@
+package com.mas.exhibitionmanagementsystem.services;
+
+import com.mas.exhibitionmanagementsystem.models.UserAccount;
+import com.mas.exhibitionmanagementsystem.repositories.UserAccountRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class UserAccountService {
+    private final UserAccountRepository userAccountRepository;
+
+    public UserAccountService(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    public boolean authenticate(String email, String password) {
+        UserAccount userAccount = userAccountRepository.findByEmail(email);
+        if (userAccount == null) return false;
+
+        return decrypt(password, userAccount.getPassword());
+    }
+
+    public boolean validateEmail(String email) {
+        String emailRegex = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+    public void register(String email, String password) {
+        UserAccount newUserAccount = new UserAccount();
+        newUserAccount.setEmail(email);
+        newUserAccount.setPassword(password);
+        userAccountRepository.save(newUserAccount);
+    }
+
+    private String encrypt(String inputPassword) {
+        return new BCryptPasswordEncoder().encode(inputPassword);
+    }
+
+    private boolean decrypt(String inputPassword, String hashedPassword) {
+        return new BCryptPasswordEncoder().matches(inputPassword, hashedPassword);
+    }
+}

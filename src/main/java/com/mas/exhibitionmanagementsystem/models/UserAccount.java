f66cb31c5e3bfcbafd63b9b5bdcbac762319ade5
lef8 <alan.sipiorski14@gmail.com>
2023-06-20 17:32:59 +0200

    set email to unique

--- src/main/java/com/mas/exhibitionmanagementsystem/models/UserAccount.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/models/UserAccount.java
@@ -10,7 +10,7 @@ public class UserAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)

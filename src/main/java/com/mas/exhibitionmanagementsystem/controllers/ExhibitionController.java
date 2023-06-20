b4355d2e961c21776ec580bffb636b78bab6b5ef
alans <alan.sipiorski14@gmail.com>
2023-06-20 23:16:17 +0200

    add comments

--- src/main/java/com/mas/exhibitionmanagementsystem/controllers/ExhibitionController.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/controllers/ExhibitionController.java
@@ -9,22 +9,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+/**
+ * Controller class for managing exhibitions
+ */
 @Controller
 public class ExhibitionController {
     private final ExhibitionService exhibitionService;
-
     public ExhibitionController(ExhibitionService exhibitionService) {
         this.exhibitionService = exhibitionService;
     }
 
+    /**
+     * Get all exhibitions and display them in the view,
+     * then check if there are exhibitions available or not
+     *
+     * @param model used for passing data to the view
+     * @return path to the view, depending on exhibitions availability
+     */
     @GetMapping("/exhibitions")
-    public String getAllOngoingExhibitions(Model model) {
+    public String getAllExhibitions(Model model) {
         List<Exhibition> exhibitions = exhibitionService.getAllExhibitions();
         if (exhibitions.isEmpty()) return "no-exhibitions";
         model.addAttribute("exhibitions", exhibitions);
         return "exhibitions";
     }
 
+    /**
+     * Get specific exhibition based on provided id
+     *
+     * @param id used for getting the desired exhibition
+     * @param model used for passing data to the view
+     * @return path to the view
+     */
     @GetMapping("exhibition/{id}")
     public String getExhibition(@PathVariable Long id, Model model) {
         Exhibition exhibition = exhibitionService.getExhibitionById(id).orElse(new Exhibition());
@@ -32,6 +48,14 @@ public class ExhibitionController {
         return "exhibition-details";
     }
 
+    /**
+     * Get all artworks related to the given exhibition
+     * and display them in the view
+     *
+     * @param id of chosen exhibition
+     * @param model used for passing data to the view
+     * @return path to the view
+     */
     @GetMapping("exhibition/{id}/artworks")
     public String getArtWorks(@PathVariable Long id, Model model) {
         Exhibition exhibition = exhibitionService.getExhibitionById(id).orElse(new Exhibition());
--- src/main/java/com/mas/exhibitionmanagementsystem/controllers/ExhibitionReservationController.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/controllers/ExhibitionReservationController.java
@@ -4,7 +4,6 @@ import com.mas.exhibitionmanagementsystem.models.Exhibition;
 import com.mas.exhibitionmanagementsystem.models.Reservation;
 import com.mas.exhibitionmanagementsystem.services.ExhibitionReservationService;
 import com.mas.exhibitionmanagementsystem.services.ExhibitionService;
-import com.mas.exhibitionmanagementsystem.services.ReservationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -13,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
+/**
+ * Controller class for managing exhibitions and reservations
+ */
 @Controller
 public class ExhibitionReservationController {
     private final ExhibitionService exhibitionService;
@@ -23,8 +25,15 @@ public class ExhibitionReservationController {
         this.exhibitionReservationService = exhibitionReservationService;
     }
 
+    /**
+     * Begin reservation process
+     *
+     * @param id of chosen exhibition
+     * @param model used for passing data to the view
+     * @return path to the view
+     */
     @GetMapping("/exhibition/{id}/reservation")
-    public String makeReservation(@PathVariable Long id, Model model) {
+    public String beginReservation(@PathVariable Long id, Model model) {
         Exhibition exhibition = exhibitionService.getExhibitionById(id).orElse(new Exhibition());
         model.addAttribute("exhibition", exhibition);
         model.addAttribute("reservationCount", 1);
@@ -32,6 +41,17 @@ public class ExhibitionReservationController {
         return "reservation";
     }
 
+    /**
+     * Checks availability of an exhibition for specific number of reservations
+     * and on the specific day
+     *
+     * @param request contains session data
+     * @param id of chosen exhibition
+     * @param reservationNumber number of requested reservations
+     * @param reservationDate date to make reservation for
+     * @param model used for passing data to the view
+     * @return path to the view reservation view or error view
+     */
     @PostMapping("/exhibition/{id}/reservation/check-availability")
     public String checkAvailability(HttpServletRequest request,
                                     @PathVariable("id") Long id,
@@ -50,9 +70,6 @@ public class ExhibitionReservationController {
 
         HttpSession session = request.getSession();
         session.setAttribute("exhibition", exhibition);
-        session.setAttribute("exhibitionName", exhibition.getName());
-        session.setAttribute("reservationCount", reservationNumber);
-        session.setAttribute("reservationDate", reservationDate);
 
         model.addAttribute("reservationCountDisabled", true);
         model.addAttribute("reservationDateDisabled", true);
@@ -64,11 +81,25 @@ public class ExhibitionReservationController {
         return "reservation";
     }
 
+    /**
+     * Continues with the reservation process, saves number of requested reservations,
+     * date for requested reservations and audio guide info to the session
+     *
+     * @param request contains session data
+     * @param reservationNumber number of requested reservations
+     * @param reservationDate date to make reservation for
+     * @param audioGuide users chosen audio guide
+     * @return path to the view reservation view or error view
+     */
     @PostMapping("/exhibition/{id}/reservation/next")
+    //TODO: id not utilized
     public String continueReservation(HttpServletRequest request,
-                                    @PathVariable("id") Long id,
-                                    @RequestParam("audio-guide") String audioGuide) {
+                                      @RequestParam("reservation-count") int reservationNumber,
+                                      @RequestParam("reservation-date") LocalDate reservationDate,
+                                      @RequestParam("audio-guide") String audioGuide) {
         HttpSession session = request.getSession();
+        session.setAttribute("reservationCount", reservationNumber);
+        session.setAttribute("reservationDate", reservationDate);
         session.setAttribute("reservationAudioGuide", audioGuide);
 
         return "redirect:/reservation/account-options";

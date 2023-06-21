5c3f83b8beb1371b6e3c7cf9072f341511738749
lef8 <alan.sipiorski14@gmail.com>
2023-06-21 14:41:09 +0200

    add comments

--- src/main/java/com/mas/exhibitionmanagementsystem/services/ReservationService.java
+++ src/main/java/com/mas/exhibitionmanagementsystem/services/ReservationService.java
@@ -11,6 +11,9 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing reservations
+ */
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
@@ -19,14 +22,30 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
+    /**
+     * Retrieves all reservations
+     * @return list of all reservations
+     */
     public List<Reservation> getAllReservations() {
         return reservationRepository.findAll();
     }
 
+    /**
+     * Retrieves reservation with given id
+     * @param id of the reservation
+     * @return Optional containing reservation or empty Optional if reservation not found
+     */
     public Optional<Reservation> getReservationById(Long id) {
         return reservationRepository.findById(id);
     }
 
+    /**
+     * Counts the number of reservations on specific exhibition for a given day
+     *
+     * @param exhibition to count reservations on
+     * @param date to count reservations for
+     * @return total number of reservations on specific exhibition for a given day
+     */
     public int countReservationsOnExhibitionByDate(Exhibition exhibition, LocalDate date) {
         List<Reservation> reservations = reservationRepository.findAllByExhibitionAndReservationDate(exhibition, date)
                 .orElse(List.of());
@@ -37,6 +56,16 @@ public class ReservationService {
         return reservationsCount;
     }
 
+    /**
+     * Creates a reservation
+     *
+     * @param date reservation date
+     * @param reservationCount number of reservations
+     * @param audioGuide specifies the audio guide option
+     * @param client making the reservation
+     * @param exhibition for which the reservation is made
+     * @return created reservation
+     */
     public Reservation makeReservation(LocalDate date,
                                        int reservationCount,
                                        AudioGuide audioGuide,
